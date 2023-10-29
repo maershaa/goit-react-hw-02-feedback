2d5a2e4a6403a3bbc32317d48fb1afcc77454a3f
maershaa <valeria.work2022@gmail.com>
2023-10-29 13:27:18 +0200

    Update Statistics2.jsx

--- src/components/Statistics/Statistics2.jsx
+++ src/components/Statistics/Statistics2.jsx
@@ -1,51 +1,51 @@
-import css from 'components/Statistics/Statistics.module.css'; 
-import React, { Component } from "react";
-import Notification from '../Notification/Notification';
+// import css from 'components/Statistics/Statistics.module.css'; 
+// import React, { Component } from "react";
+// import Notification from '../Notification/Notification';
 
 
-class Statistics2 extends Component {
+// class Statistics2 extends Component {
 
-      // Вспомогательный метод для вычисления общего количества отзывов
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.props;
-    return good + neutral + bad;
-  }
+//       // Вспомогательный метод для вычисления общего количества отзывов
+//   countTotalFeedback() {
+//     const { good, neutral, bad } = this.props;
+//     return good + neutral + bad;
+//   }
 
-  // Вспомогательный метод для вычисления процента положительных отзывов
-  countPositiveFeedbackPercentage() {
-    const { good } = this.props;
-    const totalFeedback = this.countTotalFeedback();
-    return totalFeedback > 0 ? Math.round((100 / totalFeedback) * good) : 0;
-  }
+//   // Вспомогательный метод для вычисления процента положительных отзывов
+//   countPositiveFeedbackPercentage() {
+//     const { good } = this.props;
+//     const totalFeedback = this.countTotalFeedback();
+//     return totalFeedback > 0 ? Math.round((100 / totalFeedback) * good) : 0;
+//   }
     
     
-render() {
+// render() {
 
     
 
-    return (
-      <div>
-        {totalFeedback >= 1 ? ( // Отображать блок статистики только если есть хотя бы один отзыв
-          <ul className={css.resultsContainer}>
-            <li>Good: {this.props.good}</li>
-            <li>Neutral: {this.props.neutral}</li>
-            <li>Bad: {this.props.bad}</li>
-            {/* Отображать элемент "Total" только в том случае, если good + neutral + bad >= 1, иначе его не отображать */}
-            {totalFeedback >= 1 && (
-              <li>Total: { this.countTotalFeedback()}</li>
-            )}
+//     return (
+//       <div>
+//         {totalFeedback >= 1 ? ( // Отображать блок статистики только если есть хотя бы один отзыв
+//           <ul className={css.resultsContainer}>
+//             <li>Good: {this.props.good}</li>
+//             <li>Neutral: {this.props.neutral}</li>
+//             <li>Bad: {this.props.bad}</li>
+//             {/* Отображать элемент "Total" только в том случае, если good + neutral + bad >= 1, иначе его не отображать */}
+//             {totalFeedback >= 1 && (
+//               <li>Total: { this.countTotalFeedback()}</li>
+//             )}
 
-            {/* Отображать элемент "Positive feedback" только в том случае, если процент положительных отзывов >= 1, иначе его не отображать */}
-            {positivePercentage >= 1 && (
-              <li>Positive feedback: {this.countPositiveFeedbackPercentage()}%</li>
-            )}
-          </ul>
-        ) : (
-          <Notification message="There is no feedback" /> // Выводить сообщение вместо блока статистики, если нет отзывов
-        )}
-      </div>
-    );
-  }
-}
+//             {/* Отображать элемент "Positive feedback" только в том случае, если процент положительных отзывов >= 1, иначе его не отображать */}
+//             {positivePercentage >= 1 && (
+//               <li>Positive feedback: {this.countPositiveFeedbackPercentage()}%</li>
+//             )}
+//           </ul>
+//         ) : (
+//           <Notification message="There is no feedback" /> // Выводить сообщение вместо блока статистики, если нет отзывов
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
-export default Statistics2;
+// export default Statistics2;
